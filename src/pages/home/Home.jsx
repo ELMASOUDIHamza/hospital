846aa4617fb502138d6da335f40c0eb1c0aec1d8
Hamza <elmasoudihamzajob@gmail.com>
2023-02-15 17:16:46 +0100

    Add pages folder and Home Component

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Typography from '@mui/material/Typography'
+
+const Home = () => {
+    return (
+        <div>
+            <Typography variant="h1" color="initial">Test Home Page</Typography>
+        </div>
+    );
+}
+
+export default Home;
